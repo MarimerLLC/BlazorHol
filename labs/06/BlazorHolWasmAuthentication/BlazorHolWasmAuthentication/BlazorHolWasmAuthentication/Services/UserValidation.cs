b3927d088077bcdd6ece815d9d418278ad1347cb
Rockford Lhotka <rocky@lhotka.net>
2024-10-09 00:01:53 +0200

    Improve user validator code

--- labs/06/BlazorHolWasmAuthentication/BlazorHolWasmAuthentication/BlazorHolWasmAuthentication/Services/UserValidation.cs
+++ labs/06/BlazorHolWasmAuthentication/BlazorHolWasmAuthentication/BlazorHolWasmAuthentication/Services/UserValidation.cs
@@ -4,25 +4,33 @@ public class UserValidation
 {
     public bool ValidateUser(string username, string password)
     {
-        if (username == "admin" && password == "admin")
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        var user = Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+        return user != null;
     }
 
     public List<string> GetRoles(string username)
     {
-        if (username == "admin")
+        var user = Users.FirstOrDefault(u => u.Username == username);
+        if (user == null)
         {
-            return new List<string> { "Admin" };
+            return [];
         }
         else
         {
-            return new List<string> { "User" };
+            return user.Roles;
         }
     }
+
+    private class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public List<string> Roles { get; set; } = [];
+    }
+
+    private readonly static List<User> Users =
+    [
+        new User { Username = "admin", Password = "admin", Roles = ["Admin"] },
+        new User { Username = "user", Password = "user", Roles = ["User"] }
+    ];
 }
