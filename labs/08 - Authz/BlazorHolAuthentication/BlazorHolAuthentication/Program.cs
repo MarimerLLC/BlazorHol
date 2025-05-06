47f0807cc4cec7123dd36d57c7e7329b867653fb
Rockford Lhotka <rocky@lhotka.net>
2025-05-06 17:13:03 -0500

    Add authenticationstateprovider that supports logout

--- labs/08 - Authz/BlazorHolAuthentication/BlazorHolAuthentication/Program.cs
+++ labs/08 - Authz/BlazorHolAuthentication/BlazorHolAuthentication/Program.cs
@@ -3,6 +3,7 @@ using BlazorHolAuthentication.Policies;
 using BlazorHolAuthentication.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Options;
 using System.Security.Claims;
 
@@ -12,13 +13,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddSingleton<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
 builder.Services.AddAuthorizationBuilder()
     .AddPolicy("IsAdmin", policy => policy.RequireClaim(ClaimTypes.Role, "Admin"))
     .AddPolicy("IsInEU", policy =>
         policy.Requirements.Add(new EuRequirement()));
 
 builder.Services.AddSingleton<IAuthorizationHandler, EuHandler>();
-
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
   .AddCookie();
---
+++ labs/08 - Authz/BlazorHolAuthentication/BlazorHolAuthentication/Services/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using System.Security.Claims;
+
+namespace BlazorHolAuthentication.Services
+{
+    public class CustomAuthenticationStateProvider : ServerAuthenticationStateProvider
+    {
+        private readonly ClaimsPrincipal anonymous = new(new ClaimsIdentity());
+
+        public CustomAuthenticationStateProvider()
+        {
+            SetAuthenticationState(Task.FromResult(new AuthenticationState(anonymous)));
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return base.GetAuthenticationStateAsync();
+        }
+
+        public void Logout()
+        {
+            var authenticationState = Task.FromResult(new AuthenticationState(anonymous));
+            SetAuthenticationState(authenticationState);
+            NotifyAuthenticationStateChanged(authenticationState);
+        }
+    }
+}
